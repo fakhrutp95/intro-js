6c77bb2a1faf2e49515c39c2267bd20bdd91bd14
fakhrutp95 <59821970+fakhrutp95@users.noreply.github.com>
2020-01-13 13:28:23 +0200

    changes js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log('That was it');
+var a =1;
+var b =2;
+if (a<b){
+    console.log ('a is lesser than b');
+} else{
+    console.log ('a is greater than b');
+}
+if (a==1) {
+    console.log ('a is exactly 1');
+}
+
